fe45e50f28ef93c8499d90eca4eb1fa35d2ec5db
Anna Dunster <llama747@gmail.com>
2022-02-06 15:52:16 -0800

    Add additional test to verify behavior of constructor

--- src/kata.test.ts
+++ src/kata.test.ts
@@ -13,6 +13,14 @@ describe('Post', () => {
   it('should contain a user name field', () => {
     expect(post.username).toBeDefined()
   })
+
+  it('should contain the username assigned in the constructor', () => {
+    const testPost1 = new Post('testuser1')
+    expect(testPost1.username).toEqual('testuser1')
+
+    const testPost2 = new Post('some other user')
+    expect(testPost2.username).toEqual('some other user')
+  })
 })
 
 describe('Timeline', () => {
