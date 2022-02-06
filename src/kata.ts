0ccdb1e9d65c2fce3cc34163c6f4971783d564e6
Anna Dunster <llama747@gmail.com>
2022-02-06 15:44:01 -0800

    Create empty functions for Timeline publish and retrieve. Add Post class.

--- src/kata.ts
+++ src/kata.ts
@@ -1,5 +1,11 @@
+export class Post {}
+
 export class Timeline {
-  //
+  /** Add a post to the user's timeline. */
+  publish (): void {}
+
+  /** Retrieve all posts from user's timeline. */
+  retrieve (): void {}
 }
 
 export class Wall {
